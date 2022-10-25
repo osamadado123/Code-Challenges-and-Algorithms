5af59a6e52b5160886ecb2633331bcad845385cc
osamadado123 <osama.aldado@gmail.com>
2022-10-25 12:02:47 +0000

    done

---
+++ python/code_challenges/linkedlist/challenge03/test_challenge03.py
@@ -0,0 +1,37 @@
+# Write your test here
+from challenge03 import Node,LinkedList
+def test_del_2nd_node_from_last():
+    llist = LinkedList()
+    llist.append(5)
+    llist.append(4)
+    llist.append(3)
+    llist.append(2)
+    llist.append(1)
+
+    llist.deleteNode(2)
+
+    actual =llist.printList()
+    expected = [1, 2, 3 ,5]
+    assert actual == expected
+
+def test_del_node():
+    llist = LinkedList()
+    llist.append(1)
+
+    llist.deleteNode(1)
+
+    actual =llist.printList()
+    expected = []
+    assert actual == expected
+
+def test_del_one_node():
+    llist = LinkedList()
+    llist.append(2)
+    llist.append(1)
+
+
+    llist.deleteNode(1)
+
+    actual =llist.printList()
+    expected = [1]
+    assert actual == expected
