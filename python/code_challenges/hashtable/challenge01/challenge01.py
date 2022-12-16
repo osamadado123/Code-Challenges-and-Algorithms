909a11a2f19b5ec74dea4502dffba5e51b4f5340
osamadado123 <osama.aldado@gmail.com>
2022-12-16 12:57:22 +0000

    fix

--- python/code_challenges/hashtable/challenge01/challenge01.py
+++ python/code_challenges/hashtable/challenge01/challenge01.py
@@ -1,38 +1,21 @@
-class TreeNode(object):
+class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-data = []
 
-def findTarget(root, k):
-    """
-    :type root: TreeNode
-    :type k: int
-    :rtype: bool
-    """
-    global data
-    data = []
-    if root is None:
-        return False
-    helper(root)
-    left = 0 
-    right = len(data) - 1
-    temp = data[left] + data[right]
-    while left < right:
-        if temp == k:
+
+
+def Solution(TreeNode, k):
+    Stack, HashTable = [TreeNode], set()
+    while Stack:
+        node = Stack.pop()
+        if k - node.val in HashTable: 
             return True
-        elif temp < k:
-            left += 1
-        else:
-            right -= 1
-        temp = data[left] + data[right]
+        HashTable.add(node.val)
+        if node.left: 
+            Stack.append(node.left)
+        if node.right: 
+            Stack.append(node.right)
+            
     return False
-    
-def helper(root):
-    global data
-    if root.left is not None:
-        helper(root.left)
-    data.append(root.val)
-    if root.right is not None:
-        helper(root.right)
